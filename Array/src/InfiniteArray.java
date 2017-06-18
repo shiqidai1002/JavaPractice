7f4b8b57241f859209c2e01178da846e0bb398dc
George <georgezmsje@yahoo.com>
2017-06-18 11:44:43 -0700

    Update InfiniteArray.java

--- Array/src/InfiniteArray.java
+++ Array/src/InfiniteArray.java
@@ -1,14 +1,18 @@
 
 public class InfiniteArray {
-	String[] array1;
-	String[] array2;
+	// You should only have one array stored in the object.
+	String[] data;
+	int defaultSize = 10; // or something
 	
 	//constructor
 	public InfiniteArray(){
+		// You should use the array constructor here to create your array with some default size.
 	};
 	
-	public void create(int initialSize){
-		array1 = new String[initialSize];
+	public InfiniteArray(int size) {
+		// Remember, objects can have more than one constructor
+		// We can have another constructor here for when the user wants to specify an initial size.
+		data = new String[initialSize];
 	}
 	
 	public void display(){
@@ -17,11 +21,21 @@ public class InfiniteArray {
 		}
 	}
 	
+	public String toString() {
+		// most objects have a toString method. You can write one here as well.
+	}
+	
 	public void fulsh(){
+		// You spelled "flush" incorrectly
+		// Also, instead of setting your data to null, you should create a new array with the default size
+		// This way, if the user tries to add to the array after flushing it won't crash the program.
 		array1 = null;
 	}
 	
 	public void fill(String value){
+		// This makes sense, but it is slow because we have to search through the array one by one every time
+		// we want to add something. Maybe it would be better to just store "nextIndex" as a variable inside
+		// the object? Then we can directly use nextIndex without searching, and just update it after a change.
 		int nextIndex = 0;
 		//check current occupation
 		for(int c = 0; c < c + 1; c++){
@@ -40,6 +54,9 @@ public class InfiniteArray {
 	}
 	
 	public void expendTo(int newSize){
+		// You spelled "expand" incorrectly
+		// But the truth is, we don't need these methods. We should automatically
+		// expand the array when the size is not large enough. See your test class for an example use case.
 		array2 = new String[newSize];
 		for (int i = 0; i < array1.length; i++){
 			array2[i] = array1[i];
