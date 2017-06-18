38267ebaf1dcd5f7cab8af0a508d394a25fd3024
George <georgezmsje@yahoo.com>
2017-06-18 11:45:22 -0700

    Update TestInfiniteArray.java

--- Array/src/TestInfiniteArray.java
+++ Array/src/TestInfiniteArray.java
@@ -1,21 +1,19 @@
 
 public class TestInfiniteArray {
 	public static void main(String[] args){
-		InfiniteArray test = new InfiniteArray();
-		test.create(1);
-		test.fill("my");
-		test.display();
-		test.expend(4);
-		test.fill("name");
-		test.fill("is");
-		test.fill("shiqi");
-		test.fill("dai");
-		System.out.println("After expending:");
-		test.display();
-		test.expendTo(6);
-		test.fill("!");
-		System.out.println("After expending:");
-		test.display();
+		InfiniteArray list = new InfiniteArray();
+		System.out.println(list.size());
+		list.add("lmao");
+		list.add("things");
+		list.add("omg");
+		list.add("Don't listen to girls who say they will die for you. Fried chicken actually died for you. Fried chicken is love.");
+		System.out.println(list.get(3)); // Should print the "Don't listen...." entry
+		for ( int x = 0; x < 1000; x++ ) {
+			list.add("load test");
+		}
+		System.out.println(list.get(666));
+		list.set(0, "Deus Vult"); // should set "lmao" to "Deus Vult"
+		System.out.println(list.get(0));
 	}
 }
 
